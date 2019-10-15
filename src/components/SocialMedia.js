c81d3af179c41329288ad056e1eac34c4219de45
Raul Lucero <raul.lucero27@gmail.com>
2019-10-14 23:20:43 -0700

    avoid call the interval multiple times

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -8,36 +8,40 @@ const getRandomNumber = max => {
   return Math.floor(Math.random() * Math.floor(max));
 };
 
-export const SocialMedia = () => {
-  const [number, setNumber] = useState(getRandomNumber(4));
+let interval;
 
-  setInterval(() => {
-    let newNumber = number;
-    while (newNumber === number) {
-      newNumber = getRandomNumber(4);
-    }
-    setNumber(newNumber);
-  }, 10000);
+export const SocialMedia = () => {
+  const [number, setNumber] = useState(getRandomNumber(3));
+
+  if (!interval) {
+    interval = setInterval(() => {
+      let newNumber = number;
+      while (newNumber === number) {
+        newNumber = getRandomNumber(3);
+      }
+      setNumber(newNumber);
+    }, 5000);
+  }
 
   return (
     <React.Fragment>
       <Link href="https://www.linkedin.com/in/raul-lucero" target="_blank">
         <FontAwesomeIcon
-          className={`animated infinite ${number === 1 ? "tada" : "pulse"}`}
+          className={`animated infinite ${number === 0 ? "tada" : "pulse"}`}
           icon={faLinkedinIn}
           size="2x"
         />
       </Link>
       <Link href="https://github.com/raullucero" target="_blank">
         <FontAwesomeIcon
-          className={`animated infinite ${number === 2 ? "tada" : "pulse"}`}
+          className={`animated infinite ${number === 1 ? "tada" : "pulse"}`}
           icon={faGithub}
           size="2x"
         />
       </Link>
       <Link href="mailto:null@rul.mx">
         <FontAwesomeIcon
-          className={`animated infinite ${number === 3 ? "tada" : "pulse"}`}
+          className={`animated infinite ${number === 2 ? "tada" : "pulse"}`}
           icon={faEnvelope}
           size="2x"
         />
